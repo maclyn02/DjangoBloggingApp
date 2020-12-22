3f3af55625faa97ca1ea8e15925eb4a776935917
Maclyn Afonso <>
2020-12-22 17:24:56 +0000

    add ability to create posts from admin interface

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    # Create string representation -> when you try to print a class, it will print the title instead
+    def __str__(self):
+        return str(f"{self.title} by {self.author}")
