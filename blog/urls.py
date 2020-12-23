ae1fdeca0a04b84560237318b24e0a6013d3932e
Maclyn Afonso <>
2020-12-23 15:22:05 +0000

    added form to submit a post from front-end

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:primary_key>/', views.post_details, name='post_details'),
+    path('post/new', views.post_new, name='post_new')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from .add_form import PostForm
 from .models import Post
 
 
@@ -18,3 +19,22 @@ def post_details(request, primary_key):
         'post_details': post
     }
     return render(request, 'blog/post_details.html', post_for_front_end)
+
+
+def post_new(request):
+    # If request is POST (i.e) form is being submitted
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_details', primary_key=post.pk)
+    # Else request is GET (i.e) when + button is clicked to request form rendering
+    else:
+        form = PostForm()
+        form_for_frontend = {
+            'form': form
+        }
+        return render(request, 'blog/post_edit.html', form_for_frontend)
