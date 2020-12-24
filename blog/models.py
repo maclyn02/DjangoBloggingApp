d2b952fae95050d021b4582eac2203932bc58cbc
Maclyn Afonso <>
2020-12-24 02:04:21 +0000

    updating main

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    approved = models.BooleanField(default=False)
 
     def publish(self):
         self.published_date = timezone.now()
